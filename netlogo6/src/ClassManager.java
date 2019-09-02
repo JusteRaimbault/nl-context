d1c00640cf4453c3e747267c3cc2b2ed8cb507af
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-09-02 15:32:21 +0200

    netlogo6

--- netlogo6/src/ClassManager.java
+++ netlogo6/src/ClassManager.java
@@ -9,9 +9,9 @@ public class ClassManager extends DefaultClassManager {
 	
     public void load(PrimitiveManager primitiveManager) {
        primitiveManager.addPrimitive("breed-names", new BreedNames());
-       //primitiveManager.addPrimitive("breed-singular-names", new BreedSingularNames());
+       primitiveManager.addPrimitive("breed-singular-names", new BreedSingularNames());
        primitiveManager.addPrimitive("link-breed-names", new LinkBreedNames());
-       //primitiveManager.addPrimitive("link-breed-singular-names", new LinkBreedSingularNames());
+       primitiveManager.addPrimitive("link-breed-singular-names", new LinkBreedSingularNames());
 	}
   
   
